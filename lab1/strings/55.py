09924b6c8d71ebe2fb54d10453aeded5383385d4
arkas888 <a_azamatuly@kbtu.kz>
2024-01-22 03:37:32 +0600

    grealish

---
+++ lab1/strings/55.py
@@ -0,0 +1,6 @@
+b = "Hello, World!"
+print(b[:5])
+
+#Slice From the Start 
+#By leaving out the start index, the range will start at the first character:
+#Get the characters from the start to position 5 (not included):
