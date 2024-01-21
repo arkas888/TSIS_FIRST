81fd682d50c504a266b478523bc32f7eb5f71b17
arkas888 <a_azamatuly@kbtu.kz>
2024-01-22 03:41:43 +0600

    saliba

---
+++ lab1/strings/57.py
@@ -0,0 +1,8 @@
+b = "Hello, World!"
+print(b[-5:-2])
+
+#Negative Indexing
+#Use negative indexes to start the slice from the end of the string:
+#Get the characters:
+#From: "o" in "World!" (position -5)
+#To, but not included: "d" in "World!" (position -2):
