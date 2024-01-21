8aafad3ce0d76670875d4f71f0c1971ce132a2e7
arkas888 <a_azamatuly@kbtu.kz>
2024-01-22 03:47:36 +0600

    salah

---
+++ lab1/strings/62.py
@@ -0,0 +1,5 @@
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+#The split() method splits the string into substrings if it finds instances of the separator:
+#The split() method returns a list where the text between the specified separator becomes the list items.
