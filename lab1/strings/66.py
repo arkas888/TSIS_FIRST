95bc4c81c34411ab5c9be34c056f4ca47c17e519
arkas888 <a_azamatuly@kbtu.kz>
2024-01-22 03:51:43 +0600

    alisson

---
+++ lab1/strings/66.py
@@ -0,0 +1,7 @@
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+#But we can combine strings and numbers by using the format() method!
+#The format() method takes the passed arguments, formats them, and places them in the string where the placeholders {} are:
+#Use the format() method to insert numbers into strings:
