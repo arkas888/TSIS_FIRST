6d45a248a76bc24e879196cef26629ba2ac49b95
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 18:47:50 +0600

    init

---
+++ lab2/examples/tuples/loop tuples/2.py
@@ -0,0 +1,6 @@
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+  
+#You can also loop through the tuple items by referring to their index number.
+#Use the range() and len() functions to create a suitable iterable.
