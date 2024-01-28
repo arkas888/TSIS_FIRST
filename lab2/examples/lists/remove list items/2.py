a13c99334b13011220db53ff42484042ce1e1343
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 17:23:59 +0600

    init

---
+++ lab2/examples/lists/remove list items/2.py
@@ -0,0 +1,5 @@
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+
+#If there are more than one item with the specified value, the remove() method removes the first occurance
