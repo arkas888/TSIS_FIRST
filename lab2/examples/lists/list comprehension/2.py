2856c86a44c6825720d2e6a6345d942450494e56
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 17:41:21 +0600

    init

---
+++ lab2/examples/lists/list comprehension/2.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+
+#With list comprehension you can do all that with only one line of code:
