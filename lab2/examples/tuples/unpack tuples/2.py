5952e94879a6a6d5060ff3600f16924991884443
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 18:42:19 +0600

    init

---
+++ lab2/examples/tuples/unpack tuples/2.py
@@ -0,0 +1,9 @@
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+#But, in Python, we are also allowed to extract the values back into variables. This is called "unpacking":
