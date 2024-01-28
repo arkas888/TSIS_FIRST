8080f65784a08037195fe7f63415156de8903d79
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 16:07:19 +0600

    init

---
+++ lab2/examples/booleans/10.py
@@ -0,0 +1,5 @@
+x = 200
+print(isinstance(x, int)) 
+
+#Python also has many built-in functions that return a boolean value, 
+#like the isinstance() function, which can be used to determine if an object is of a certain data type:
