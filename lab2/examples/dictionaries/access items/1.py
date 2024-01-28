ebadcaf1b2d18844a5216b50c36ce080f0e28d0d
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:24:35 +0600

    init

---
+++ lab2/examples/dictionaries/access items/1.py
@@ -0,0 +1,9 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+
+#You can access the items of a dictionary by referring to its key name, inside square brackets:
+#Get the value of the "model" key:
