6e741bcf3b40656e0c8d3cfaff2f14042f9e48f7
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:49:43 +0600

    init

---
+++ lab2/examples/for_loops/8.py
@@ -0,0 +1,6 @@
+for x in range(2, 30, 3):
+  print(x)
+  
+#The range() function defaults to increment the sequence by 1, 
+# however it is possible to specify the increment value by adding a third parameter: range(2, 30, 3):
+#Increment the sequence with 3 (default is 1):  
