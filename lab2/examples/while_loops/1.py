a2e21d6a131243afc267e543a6eb2e6374b75e73
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:35:05 +0600

    init

---
+++ lab2/examples/while_loops/1.py
@@ -0,0 +1,9 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+  
+#With the while loop we can execute a set of statements as long as a condition is true.
+#Print i as long as i is less than 6:
+#remember to increment i, or else the loop will continue forever.
+#The while loop requires relevant variables to be ready, in this example we need to define an indexing variable, i, which we set to 1.
