0814cfecb38685efe382a17b6f908c21dee4b184
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:10:38 +0600

    init

---
+++ lab2/examples/dictionaries/copy dictionaries/2.py
@@ -0,0 +1,10 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+#Another way to make a copy is to use the built-in function dict().
+#Make a copy of a dictionary with the dict() function:
