ab1125e2799e047ebf0dde3e97261a00b96351a6
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:27:00 +0600

    init

---
+++ lab2/examples/if...else/6.py
@@ -0,0 +1,8 @@
+a = 2
+b = 330
+
+print("A") if a > b else print("B")
+
+#If you have only one statement to execute, one for if, and one for else, you can put it all on the same line:
+#One line if else statement:
+#This technique is known as Ternary Operators, or Conditional Expressions.
