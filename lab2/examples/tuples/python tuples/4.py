3a1bdabdb8562515bf0904d2aa8a69f9c593f935
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 18:17:46 +0600

    init

---
+++ lab2/examples/tuples/python tuples/4.py
@@ -0,0 +1,8 @@
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+#To create a tuple with only one item, you have to add a comma after the item, otherwise Python will not recognize it as a tuple.
