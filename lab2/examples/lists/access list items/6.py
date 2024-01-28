c1480a68d43c693e04d5d5127dd76304aa594148
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 16:32:22 +0600

    init

---
+++ lab2/examples/lists/access list items/6.py
@@ -0,0 +1,4 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+#Specify negative indexes if you want to start the search from the end of the list
