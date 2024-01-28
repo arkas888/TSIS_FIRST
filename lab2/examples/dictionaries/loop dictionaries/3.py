332f4f89436008094c54a6839cc8dd536734d413
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:06:53 +0600

    init

---
+++ lab2/examples/dictionaries/loop dictionaries/3.py
@@ -0,0 +1,9 @@
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict.values():
+  print(x)
+  
+  #You can also use the values() method to return values of a dictionary:
