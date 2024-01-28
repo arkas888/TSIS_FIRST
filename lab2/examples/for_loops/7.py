dcfbe9ea50f261f77314ea64e02b2367232b8dda
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:48:45 +0600

    init

---
+++ lab2/examples/for_loops/7.py
@@ -0,0 +1,6 @@
+for x in range(2, 6):
+  print(x)
+
+#The range() function defaults to 0 as a starting value, however it is possible to specify the starting value by adding a parameter: 
+# range(2, 6), which means values from 2 to 6 (but not including 6):
+  
