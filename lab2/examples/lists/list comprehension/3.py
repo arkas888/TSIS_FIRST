b56934d0e961dc4b361f8669ea206794734688d4
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 17:42:40 +0600

    init

---
+++ lab2/examples/lists/list comprehension/3.py
@@ -0,0 +1,4 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if x != "apple"]
+
+#The condition is like a filter that only accepts the items that valuate to True.
