7382606c282e39a0e21ed02b2997bf1e8f4639f0
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:05:05 +0600

    init

---
+++ lab2/examples/sets/join sets/4.py
@@ -0,0 +1,9 @@
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.intersection(y)
+
+print(z)
+
+#The intersection() method will return a new set, that only contains the items that are present in both sets.
+#Return a set that contains the items that exist in both set x, and set y:
