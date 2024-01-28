e62f31655efe48ead9d0a1cfff6246f74d8ec059
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 20:39:15 +0600

    init

---
+++ lab2/examples/sets/access set items/1.py
@@ -0,0 +1,8 @@
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+  
+#You cannot access items in a set by referring to an index or a key.
+#But you can loop through the set items using a for loop, or ask if a specified value is present in a set, by using the in keyword.
+#Loop through the set, and print the values:
