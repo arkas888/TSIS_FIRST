5f9fa47e7cb3ff1d11099b37943234dbe9bf2aa4
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 20:40:04 +0600

    init

---
+++ lab2/examples/sets/access set items/2.py
@@ -0,0 +1,6 @@
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+
+#Check if "banana" is present in the set:
+#Once a set is created, you cannot change its items, but you can add new items.
