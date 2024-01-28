e6b2b0a3ab87cc5d1abcfe9222a43eb744285c3e
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:00:55 +0600

    init

---
+++ lab2/examples/dictionaries/remove items/1.py
@@ -0,0 +1,10 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+#There are several methods to remove items from a dictionary:
+#The pop() method removes the item with the specified key name:
