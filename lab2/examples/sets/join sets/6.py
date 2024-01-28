8b3908c43f1c5a45042ac2a72c0d39059c565936
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:06:14 +0600

    init

---
+++ lab2/examples/sets/join sets/6.py
@@ -0,0 +1,9 @@
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.symmetric_difference(y)
+
+print(z)
+
+#The symmetric_difference() method will return a new set, that contains only the elements that are NOT present in both sets.
+#Return a set that contains all items from both sets, except items that are present in both:
