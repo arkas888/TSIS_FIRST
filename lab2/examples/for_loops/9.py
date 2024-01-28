3b58faa995cbc790e9d923f8612919434fb5e607
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:51:14 +0600

    init

---
+++ lab2/examples/for_loops/9.py
@@ -0,0 +1,7 @@
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+  
+#The else keyword in a for loop specifies a block of code to be executed when the loop is finished:
+#Print all numbers from 0 to 5, and print a message when the loop has ended:  
