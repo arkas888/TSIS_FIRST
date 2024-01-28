ea6ac3d16bed25f2d6f5d7de7edecf96cf0e40e5
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 18:01:40 +0600

    init

---
+++ lab2/examples/lists/sorts lists/8.py
@@ -0,0 +1,7 @@
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+
+#What if you want to reverse the order of a list, regardless of the alphabet?
+#The reverse() method reverses the current sorting order of the elements.
+#Reverse the order of the list items:
