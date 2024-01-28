15d48825e01b7515db196ecd08031b842d6d8a5a
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:32:39 +0600

    init

---
+++ lab2/examples/if...else/12.py
@@ -0,0 +1,9 @@
+a = 33
+b = 200
+
+if b > a:
+  pass
+
+# having an empty if statement like this, would raise an error without the pass statement
+#if statements cannot be empty, but if you for some reason have an if statement with no content, 
+#put in the pass statement to avoid getting an error.
