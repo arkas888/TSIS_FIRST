7e14312a4aa9e27e6ff6d5224766e23f1cd193cb
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:07:23 +0600

    ini

---
+++ lab2/examples/dictionaries/loop dictionaries/4.py
@@ -0,0 +1,9 @@
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict.keys():
+  print(x)
+  
+  #You can use the keys() method to return the keys of a dictionary:
