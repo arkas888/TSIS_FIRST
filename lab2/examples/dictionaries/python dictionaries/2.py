86db06aa9e267a8d5414d85007d1e5027b72693b
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:11:51 +0600

    init

---
+++ lab2/examples/dictionaries/python dictionaries/2.py
@@ -0,0 +1,10 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+#Dictionary items are ordered, changeable, and does not allow duplicates.
+#Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
+#Print the "brand" value of the dictionary:
