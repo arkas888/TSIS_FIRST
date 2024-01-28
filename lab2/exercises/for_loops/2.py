57fa4c014488e140bb5ed9494028da1cdc0e56bb
arkas888 <a_azamatuly@kbtu.kz>
2024-01-29 00:11:39 +0600

    init

---
+++ lab2/exercises/for_loops/2.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+  
+#In the loop, when the item value is "banana", jump directly to the next item. 
