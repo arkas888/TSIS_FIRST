16e0f8b136fb69066e79456863f30b4f0f162da7
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 20:50:20 +0600

    init

--- lab2/examples/sets/remove set items/2.py
+++ lab2/examples/sets/remove set items/2.py
@@ -4,5 +4,5 @@ thisset.discard("banana")
 
 print(thisset)
 
-#Note: If the item to remove does not exist, remove() will raise an error.
 #Remove "banana" by using the discard() method:
+#Note: If the item to remove does not exist, discard() will NOT raise an error.
