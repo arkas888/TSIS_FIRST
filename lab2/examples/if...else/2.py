f73f9e5788a1f23057ac96d7f898ea3d54145c65
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:20:30 +0600

    init

---
+++ lab2/examples/if...else/2.py
@@ -0,0 +1,10 @@
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+  
+#The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+#In this example a is equal to b, so the first condition is not true,
+# but the elif condition is true, so we print to screen that "a and b are equal".
