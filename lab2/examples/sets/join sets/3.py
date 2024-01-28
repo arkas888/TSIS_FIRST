acc2da1b4a8987f96cd4cef7a488909b46c2872f
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:01:44 +0600

    init

---
+++ lab2/examples/sets/join sets/3.py
@@ -0,0 +1,9 @@
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.intersection_update(y)
+
+print(x)
+
+#The intersection_update() method will keep only the items that are present in both sets.
+#Keep the items that exist in both set x, and set y:
