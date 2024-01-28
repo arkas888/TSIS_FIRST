ec3e483236741e127020bcc5dd4b5ec639ebf765
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:51:23 +0600

    init

---
+++ lab2/examples/dictionaries/add items/2.py
@@ -0,0 +1,10 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
+
+#The update() method will update the dictionary with the items from a given argument. If the item does not exist, the item will be added.
+#The argument must be a dictionary, or an iterable object with key:value pairs.
+#Add a color item to the dictionary by using the update() method:
