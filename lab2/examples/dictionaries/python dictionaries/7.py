9b0d2afe9bcd9bc3a808e8bc11c2ab4a83dbbcea
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:19:43 +0600

    init

---
+++ lab2/examples/dictionaries/python dictionaries/7.py
@@ -0,0 +1,5 @@
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
+
+#It is also possible to use the dict() constructor to make a dictionary.
+#Using the dict() method to make a dictionary:
