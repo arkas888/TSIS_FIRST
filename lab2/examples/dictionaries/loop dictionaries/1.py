9f4b8634f57f1b5f538de3533ab6426653b5b435
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:05:11 +0600

    init

---
+++ lab2/examples/dictionaries/loop dictionaries/1.py
@@ -0,0 +1,12 @@
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(x)
+
+#You can loop through a dictionary by using a for loop.
+#When looping through a dictionary, the return value are the keys of the dictionary,
+# but there are methods to return the values as well.
+#Print all key names in the dictionary, one by one:
