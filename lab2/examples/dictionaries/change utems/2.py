796f3a30c8f4faff45ecf158cb739f223e6b1e38
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:47:35 +0600

    init

---
+++ lab2/examples/dictionaries/change utems/2.py
@@ -0,0 +1,10 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+
+#The update() method will update the dictionary with the items from the given argument.
+#The argument must be a dictionary, or an iterable object with key:value pairs.
+#Update the "year" of the car by using the update() method:
