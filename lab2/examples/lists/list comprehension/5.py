c7a3b2c48770571baf592325f63b73065625939f
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 17:46:02 +0600

    init

---
+++ lab2/examples/lists/list comprehension/5.py
@@ -0,0 +1,6 @@
+newlist = [x for x in range(10)]
+
+print(newlist)
+
+#The iterable can be any iterable object, like a list, tuple, set etc.
+#You can use the range() function to create an iterable:
