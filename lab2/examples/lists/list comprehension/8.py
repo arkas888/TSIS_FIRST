4dced5691c21982eb29e609a4370df03cb2850da
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 17:48:57 +0600

    init

---
+++ lab2/examples/lists/list comprehension/8.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = ['hello' for x in fruits]
+
+print(newlist)
+
+#You can set the outcome to whatever you like:
+#Set all values in the new list to 'hello':
