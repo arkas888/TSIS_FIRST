04c6f2292f80d6c7197929fc17d7397be7f86d03
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 18:09:25 +0600

    init

---
+++ lab2/examples/lists/join lists/3.py
@@ -0,0 +1,7 @@
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
+
+#Or you can use the extend() method, where the purpose is to add elements from one list to another list
