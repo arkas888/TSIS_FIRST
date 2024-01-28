66f151f48a903cc91eaeceef458d791ac70fdd97
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:51:58 +0600

    init

---
+++ lab2/examples/for_loops/10.py
@@ -0,0 +1,8 @@
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+  
+#Note: The else block will NOT be executed if the loop is stopped by a break statement.
+#Break the loop when x is 3, and see what happens with the else block:  
