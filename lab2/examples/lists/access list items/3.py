543a95ede6f4003b4fade8c10df85c1662dcd469
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 16:29:04 +0600

    init

---
+++ lab2/examples/lists/access list items/3.py
@@ -0,0 +1,5 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+#You can specify a range of indexes by specifying where to start and where to end the range.
+#When specifying a range, the return value will be a new list with the specified items.
