c00c24bcab8c796e337ad331080790253ce3e1a2
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:53:58 +0600

    init

---
+++ lab2/examples/for_loops/12.py
@@ -0,0 +1,6 @@
+for x in [0, 1, 2]:
+  pass
+
+# having an empty for loop like this, would raise an error without the pass statement
+#for loops cannot be empty, but if you for some reason have a for loop with no content,
+# put in the pass statement to avoid getting an error.
