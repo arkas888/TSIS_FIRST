34891e9fc49c47f57ec63ab5a838c39916b6c4cb
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:45:52 +0600

    init

---
+++ lab2/examples/dictionaries/change utems/1.py
@@ -0,0 +1,9 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+
+#You can change the value of a specific item by referring to its key name:
+#Change the "year" to 2018:
