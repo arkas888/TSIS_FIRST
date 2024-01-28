80066858d41d7ce61926a46db9e149e004ee14bd
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 17:33:05 +0600

    init

---
+++ lab2/examples/lists/loop lists/2.py
@@ -0,0 +1,6 @@
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+  
+#You can also loop through the list items by referring to their index number.
+#Use the range() and len() functions to create a suitable iterable.
