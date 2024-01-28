214a092823f2bb379a63d0bde5f0b879df263a50
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 17:59:08 +0600

    init

---
+++ lab2/examples/lists/sorts lists/6.py
@@ -0,0 +1,6 @@
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
+
+#By default the sort() method is case sensitive, resulting in all capital letters being sorted before lower case letters
+#Case sensitive sorting can give an unexpected result:
