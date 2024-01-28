aeb2e4e85fa13c6a2904eb5175577ae411a77e41
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 18:36:34 +0600

    init

---
+++ lab2/examples/tuples/update tuples/3.py
@@ -0,0 +1,8 @@
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+
+#2. Add tuple to a tuple. You are allowed to add tuples to tuples, 
+# so if you want to add one item, (or many), create a new tuple with the item(s), and add it to the existing tuple:
