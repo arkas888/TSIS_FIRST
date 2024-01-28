401ec28bdd9cdc0f630913060f5cd7b13ddfcf45
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 16:42:53 +0600

    init

---
+++ lab2/examples/lists/add list items/2.py
@@ -0,0 +1,6 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+#To insert a list item at a specified index, use the insert() method.
+#The insert() method inserts an item at the specified index
