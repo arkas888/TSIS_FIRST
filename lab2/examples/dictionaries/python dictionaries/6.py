3018f7d63fdf6c79d9a8a70f3575370bde83d2d5
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:18:55 +0600

    init

---
+++ lab2/examples/dictionaries/python dictionaries/6.py
@@ -0,0 +1,9 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+#From Python's perspective, dictionaries are defined as objects with the data type 'dict':
+#Print the data type of a dictionary:
