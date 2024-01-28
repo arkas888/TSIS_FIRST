6fa282b6641a309d397431940e2a2b512ba2a0bc
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:36:02 +0600

    init

---
+++ lab2/examples/dictionaries/access items/7.py
@@ -0,0 +1,15 @@
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.values()
+
+print(x) #before the change
+
+car["color"] = "red"
+
+print(x) #after the change
+
+#Add a new item to the original dictionary, and see that the values list gets updated as well:
