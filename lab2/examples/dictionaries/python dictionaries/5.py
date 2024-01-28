5cf2f396c7d212f65e077897456646db5bdec11f
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:18:06 +0600

    init

---
+++ lab2/examples/dictionaries/python dictionaries/5.py
@@ -0,0 +1,11 @@
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+print(thisdict)
+
+#The values in dictionary items can be of any data type:
+#String, int, boolean, and list data types:
