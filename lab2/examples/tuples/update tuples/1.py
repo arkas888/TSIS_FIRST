79bceed894bede7035ac4970f69af4e11f1b543f
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 18:34:13 +0600

    init

---
+++ lab2/examples/tuples/update tuples/1.py
@@ -0,0 +1,9 @@
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+#Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
+#But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.
--- lab2/examples/tuples/update tuples/5.py
+++
@@ -1,9 +0,0 @@
-x = ("apple", "banana", "cherry")
-y = list(x)
-y[1] = "kiwi"
-x = tuple(y)
-
-print(x)
-
-#Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
-#But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.
