99e7e850d3f879e848fb22f4a83390fede3c432e
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 20:30:46 +0600

    init

---
+++ lab2/examples/sets/python sets/3.py
@@ -0,0 +1,6 @@
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
+
+#Note: The values True and 1 are considered the same value in sets, and are treated as duplicates:
+#True and 1 is considered the same value:
