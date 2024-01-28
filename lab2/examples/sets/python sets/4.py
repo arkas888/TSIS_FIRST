e91a1f651c6ec9e4466c41ae2d76ff69a3e462e6
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 20:32:04 +0600

    init

---
+++ lab2/examples/sets/python sets/4.py
@@ -0,0 +1,6 @@
+thisset = {"apple", "banana", "cherry", False, True, 0}
+
+print(thisset)
+
+#False and 0 is considered the same value:
+#Note: The values False and 0 are considered the same value in sets, and are treated as duplicates:
