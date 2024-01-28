2db10b42d7e8484eb565e9dbbacaa352d1dbcdaa
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 20:50:39 +0600

    init

--- lab2/examples/sets/remove set items/1.py
+++ lab2/examples/sets/remove set items/1.py
@@ -6,3 +6,4 @@ print(thisset)
 
 #To remove an item in a set, use the remove(), or the discard() method.
 #Remove "banana" by using the remove() method:
+#Note: If the item to remove does not exist, remove() will raise an error.
