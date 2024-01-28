9559b74b1a463094d7351c9c2ada655fdd070eb2
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 20:51:47 +0600

    init

---
+++ lab2/examples/sets/remove set items/3.py
@@ -0,0 +1,12 @@
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+
+print(thisset)
+
+#You can also use the pop() method to remove an item, but this method will remove a random item, so you cannot be sure what item that gets removed.
+#The return value of the pop() method is the removed item.
+#Remove a random item by using the pop() method:
+#Note: Sets are unordered, so when using the pop() method, you do not know which item that gets removed.
