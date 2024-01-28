e477bf65ac92e13ff6b71213c7032f25c4e2e070
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 17:57:37 +0600

    init

---
+++ lab2/examples/lists/sorts lists/5.py
@@ -0,0 +1,10 @@
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+#You can also customize your own function by using the keyword argument key = function.
+#The function will return a number that will be used to sort the list (the lowest number first):
+#Sort the list based on how close the number is to 50:
