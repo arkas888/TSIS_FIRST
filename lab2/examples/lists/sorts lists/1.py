eabd296d3abed0b5cb421057f29ba5b4578c6f06
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 17:53:00 +0600

    init

---
+++ lab2/examples/lists/sorts lists/1.py
@@ -0,0 +1,5 @@
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+#List objects have a sort() method that will sort the list alphanumerically, ascending, by default:
