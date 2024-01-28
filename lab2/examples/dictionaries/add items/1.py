959d4995c5717b36ac11570ebe75b2c288b13ec7
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:50:23 +0600

    init

---
+++ lab2/examples/dictionaries/add items/1.py
@@ -0,0 +1,9 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+#Adding an item to the dictionary is done by using a new index key and assigning a value to it:
