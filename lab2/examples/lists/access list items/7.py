914bf94b2511d8192736488ece266f205791e435
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 16:33:03 +0600

    init

---
+++ lab2/examples/lists/access list items/7.py
@@ -0,0 +1,5 @@
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+  
+  #To determine if a specified item is present in a list use the in keyword
