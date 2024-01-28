97c015e0a9f1a1765487183461efb11da6499839
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 20:43:29 +0600

    init

--- lab2/examples/sets/add set items/3.py
+++
@@ -1,9 +0,0 @@
-thisset = {"apple", "banana", "cherry"}
-
-thisset.add("orange")
-
-print(thisset)
-
-#Once a set is created, you cannot change its items, but you can add new items.
-#To add one item to a set use the add() method.
-#Add an item to a set, using the add() method:
