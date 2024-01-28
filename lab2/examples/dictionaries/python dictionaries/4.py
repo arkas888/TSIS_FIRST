4b51e7e149ec9a1c5a0505d3b4d996922d949d2c
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:15:32 +0600

    init

---
+++ lab2/examples/dictionaries/python dictionaries/4.py
@@ -0,0 +1,10 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(len(thisdict))
+
+#To determine how many items a dictionary has, use the len() function:
+#Print the number of items in the dictionary:
