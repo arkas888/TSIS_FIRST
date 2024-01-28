03625e91b2eac7952d6bc97d400f5d801b5aa20c
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 18:14:18 +0600

    init

---
+++ lab2/examples/tuples/python tuples/1.py
@@ -0,0 +1,13 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+#Tuples are used to store multiple items in a single variable.
+#Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+#A tuple is a collection which is ordered and unchangeable.
+#Tuples are written with round brackets.
+
+#theory
+#List is a collection which is ordered and changeable. Allows duplicate members.
+#Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+#Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+#Dictionary is a collection which is ordered** and changeable. No duplicate members.
