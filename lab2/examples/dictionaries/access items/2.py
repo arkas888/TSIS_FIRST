bad7894e5ba487eda65f5b128aa82cab55469fcc
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 21:28:17 +0600

    init

--- lab2/examples/dictionaries/access items/2.py
+++ lab2/examples/dictionaries/access items/2.py
@@ -1,4 +1,11 @@
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
 x = thisdict.get("model")
+print(x)
+
 
 #Get the value of the "model" key:
 #There is also a method called get() that will give you the same result:
