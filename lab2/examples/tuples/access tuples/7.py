090c45eb91688984f0eb2e8d694f82217937e3b1
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 18:29:32 +0600

    init

---
+++ lab2/examples/tuples/access tuples/7.py
@@ -0,0 +1,5 @@
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+
+#To determine if a specified item is present in a tuple use the in keyword:
