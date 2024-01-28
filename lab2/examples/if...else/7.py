ba7cabad40ddd208f9901530ef8701eaf7040cbd
arkas888 <a_azamatuly@kbtu.kz>
2024-01-28 22:27:53 +0600

    init

---
+++ lab2/examples/if...else/7.py
@@ -0,0 +1,7 @@
+a = 330
+b = 330
+
+print("A") if a > b else print("=") if a == b else print("B")
+
+#You can also have multiple else statements on the same line:
+#One line if else statement, with 3 conditions:
